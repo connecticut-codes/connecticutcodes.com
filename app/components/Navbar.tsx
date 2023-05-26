907b455e9fe8e67c59ae94216269adedb34818af
BryonVerdone <bryonverdone@gmail.com>
2023-05-26 16:07:10 -0400

    sdded responsive nav/mobile menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="sticky top-0  bg-gray-800">
+      <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
@@ -97,22 +97,22 @@ const Navbar = () => {
           <div className="px-2 pt-2 pb-3 space-y-1 bg-gray-900">
             {/* Replace the '#' with your actual link destinations */}
             <a
-              href="/"
+              href="/about"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               About
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Events
             </a>
             <a
               href="/events"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               Events
+            </a>
+            <a
+              href="/blog"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Blog
             </a>
           </div>
         </div>
