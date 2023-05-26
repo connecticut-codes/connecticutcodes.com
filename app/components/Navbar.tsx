0c56bdf92c80fdd18f0ad452fb2dca8217fc8c9b
BryonVerdone <bryonverdone@gmail.com>
2023-05-26 13:03:16 -0400

    added nav with links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,122 @@
+"use client"
 import Link from 'next/link';
+  
+import React, { useState } from 'react';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className='flex justify-around'>
-      <Link href='/'>Logo</Link>
-      <ul className=' flex gap-4 justify-between'>
-        <Link href='/about'>
-          <li>about</li>
-        </Link>
-        <Link href='/events'>
-          <li>events</li>
-        </Link>
-      </ul>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          {/* Logo */}
+          <div className="flex-shrink-0 flex items-center">
+            {/* Replace 'Logo' with your actual logo */}
+            <Link href='/' className="text-white font-bold text-lg">Logo</Link>
+          </div>
+
+          {/* Menu button for mobile */}
+          <div className="flex items-center -mr-2 sm:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+
+          {/* Links */}
+          <div className={`hidden sm:flex sm:items-center sm:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
+            {/* Replace the '#' with your actual link destinations */}
+            <Link
+              href="/about"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              About
+            </Link>
+            <a
+              href="/events"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Events
+            </a>
+            <a
+              href="/blog"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Blog
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile menu */}
+      {isOpen && (
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 bg-gray-900">
+            {/* Replace the '#' with your actual link destinations */}
+            <a
+              href="/"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Events
+            </a>
+            <a
+              href="/events"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Events
+            </a>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
